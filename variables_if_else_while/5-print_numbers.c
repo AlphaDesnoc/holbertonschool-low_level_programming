272383e717267c53addbc9c304697475de059260
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 11:42:30 +0200

    Task 05

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
