d07cd002906e3515261f7b15597c880d4c0293fd
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 13:54:47 +0200

    Task 08

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* jack_bauer - Entry point
 * 
-* Return: Always 0 (Success)
+* Return: void
 */
 
-int main(void)
+void jack_bauer(void)
 {
-	return (0);
+	char i;
+	char j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		
+		while (j < 60)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			_putchar(':');
+			_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
 }
