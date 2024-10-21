49ac09102c511615a51e8709b8c937085a8686a7
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 10:38:36 +0200

    Task 00

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("is zero\n");
 	}
 	else if (n < 0)
 	{
-		printf("is negative");
+		printf("is negative\n");
 	}
 	return (0);
 }
