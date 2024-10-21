8e6d9df1caa06f44df1dc1098daeb427bb4b26bc
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 11:56:09 +0200

    Task 07

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 122;
+
+	while (c > 96)
+	{
+		putchar(c);
+		c--;
+	}
+	
+	putchar(10);
+
+	return (0);
+}
