af35be0ed898ca8b8640c51a44968a5e5ac72b4f
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 12:53:22 +0200

    Task 01

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet lowercase
- *
- * Return: void
- */
-
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c < 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
-	_putchar(c);
+
+	_putchar('\n');
 }
