188d795308d01b4eba6552b1549b75cbdad94b93
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 11:40:22 +0200

    Task 04

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 97;
+
+	while (c < 123)
+	{
+		if (c != 101 && c != 113)
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar(10);
+
+	return (0);
+}
