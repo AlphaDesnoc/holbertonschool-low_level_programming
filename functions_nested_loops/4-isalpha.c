4fcee6e241addaa09a25c40bf48169adce052080
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 13:19:26 +0200

    Task 04

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
+* _isalpha - Entry point
+*
+* @c: c is integer
+*
+* Return: 1 (true) or 0 (false)
 */
 
-int main(void)
+int _isalpha(int c)
 {
-	return (0);
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
