190fc73683827f57f090bb08599c1fb38cfc1a39
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 12:07:56 +0200

    Task 09

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	short int c = 0;
+
+	while (c < 10)
+	{
+		putchar('0' + c);
+		putchar(44);
+		putcahr(32);
+		c++;
+	}
+	putchar(10);
+
+	return (0);
+}
