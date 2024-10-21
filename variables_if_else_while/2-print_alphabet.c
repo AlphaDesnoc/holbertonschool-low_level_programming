bdaf141ee058c11fdb64b02cf54dd0bb02201aba
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 10:58:45 +0200

    Task 02

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c < 123)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
