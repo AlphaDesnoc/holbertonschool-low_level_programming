696e20b5fb365f5579f7d143c079ad2116be950a
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 11:52:19 +0200

    Task 06

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	short int c = 0;
+
+	while (c < 10)
+	{
+		putchar('0' + c);
+		c++;
+	}
+	putchar(10);
+
+	return (0);
+}
