f04822641028c6907327da9c424d6ee6dbff0f53
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 12:00:52 +0200

    Task 08

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		putchar('0' + c);
+		c++;
+	}
+	c = 97;
+	while (c < 103)
+	{
+		putchar(c);
+	}
+	putchar(10);
+
+	return (0);
+}
