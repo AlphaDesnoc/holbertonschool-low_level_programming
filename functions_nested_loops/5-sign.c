1d5da59a64dd14c4e6fa4bbae1cb8613f545dc17
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-21 13:33:01 +0200

    Task 05

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,12 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
+* print_sign - Entry point
+* @n: n is integer
+* Return: 1 (greater than 0) or 0 (is zero) or -1 (less thanzero
 */
 
-int main(void)
+int print_sign(int n)
 {
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 	return (0);
 }
