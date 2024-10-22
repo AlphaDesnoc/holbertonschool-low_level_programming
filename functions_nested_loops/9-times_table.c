d87a7936c2b2c1e5659660994b2b704ed916525e
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-22 09:15:13 +0200

    Task 09

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,9 +18,25 @@ void times_table(void)
 		{
 			k = i * j;
 
-			_putchar('0' + k);
-			_putchar(44);
-			_putchar(32);
+			if (j == 0)
+			{
+				_putchar('0' + k);
+			}
+			else if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (k / 10));
+				_putchar('0' + (k % 10));
+			}
 		}
+		_putchar('\n');
 	}	
 }
