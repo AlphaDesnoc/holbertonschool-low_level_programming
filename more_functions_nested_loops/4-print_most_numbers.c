b5d6e7847b3a0aa86f8d98affc17d19d4ff4ca76
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-28 09:51:52 +0100

    Task

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
+* print_most_numbers - Entry point
 * 
-* Return: Always 0 (Success)
+* Return: void
 */
 
-int main(void)
+void print_most_numbers(void)
 {
-	return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i != 2) || (i != 4))
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
 }
