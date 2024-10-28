28ad9cdb61a7e81ad52b60900ef7ce6c2a67f424
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-28 11:32:07 +0100

    Task 09

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i == 0)
-		{
-			printf("0 ");
-		}
-		else if ((i % 3 == 0) && (i % 5 != 0))
+
+		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("Fizz ");
 		}
