c4ab90e4e983706c59ad8bb149ef75dd1f86fdbd
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-28 09:18:35 +0100

    Task

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
+* _isdigit - Entry point
+* @c: parameter
+* Return: 0 or 1
 */
 
-int main(void)
+int _isdigit(int c)
 {
+	if (c >= 48 && c <= 58)
+		return (1);
 	return (0);
 }
