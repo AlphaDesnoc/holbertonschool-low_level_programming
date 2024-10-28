56b9412af390e65614973b8d09bf3d2a4d25413b
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-28 10:52:43 +0100

    Task 07

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int len, space;
+	int length;
+	int space;
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (length = 0; length < n; length++)
 		{
-			for (space = 0; space < len; space++)
+			for (space = 0; space < length; space++)
 				_putchar(' ');
 
 			_putchar('\\');
 
-			if (len == (n - 1))
+			if (length == (n - 1))
 				continue;
 			_putchar('\n');
 		}
