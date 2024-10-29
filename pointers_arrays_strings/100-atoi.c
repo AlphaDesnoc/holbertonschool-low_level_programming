f3318bd780091508e28ec49dd363daa92c3d210d
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-29 10:25:51 +0100

    Task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,41 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
+* _atoi - Entry point
+* @s: string
+* Return: int
 */
 
-int main(void)
+int _atoi(char *s)
 {
-	return (0);
+	int i;
+	int j;
+	int k;
+
+	j = 0;
+	k = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			k *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (j < 0)
+				j = (j * 10) - (s[i] - '0');
+
+			else
+				j = (s[i] - '0') * -1;
+
+			if ((s[i] + 1) < 48 || (s[i] + 1) > 57)
+				break;
+		}
+
+		if (k < 0)
+			j *= -1;
+
+	}
+
+	return (j);
 }
