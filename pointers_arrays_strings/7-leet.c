1ef555f00cc5b826258627669f45df20aef63bea
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-29 12:52:51 +0100

    Task

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
+* leet - Entry point
+* @s: p
+* Return: char
 */
 
-int main(void)
+char *leet(char *s)
 {
-	return (0);
+	int i;
+	int j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4422007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+
+	return (s);
 }
