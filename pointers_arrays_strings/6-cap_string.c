3a5e19b4e0afd44e377caf9440fd3af5bc22e82c
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-29 12:36:38 +0100

    Task

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,19 +11,19 @@ char *cap_string(char *s)
 	int i;
 	int j;
 
-	char spe_char[13] = {' ', '\t', '\n', ',', ';', '.',
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-		}
+		{
 			s[i] -= 32;
 		}
 
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == spe_char[j])
+			if (s[i] == spe[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
