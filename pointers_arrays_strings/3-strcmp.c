7cddc09e5b1fd7aad3360c7c8255f3b7d08989d9
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-29 11:56:00 +0100

    Task

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* 
+* _strcmp - Entry point
+* @s1: p
+* @s2: p
 * Return: Always 0 (Success)
 */
 
-int main(void)
+int _strcmp(char *s1, char *s2)*
 {
-	return (0);
+	int len1 = 0;
+	int len2 = 0;
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (len1 == len2)
+		return (0);
+	
+	else if (len1 > len2)
+		return (1);
+
+	else if (len2 > len1)
+		return (-1);
+
 }
