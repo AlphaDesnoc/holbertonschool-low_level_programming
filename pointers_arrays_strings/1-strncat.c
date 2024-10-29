c6ffe7938f9cdc89f33def97100e4c24bc202085
root <root@DESKTOP-VS17LS8.localdomain>
2024-10-29 11:42:14 +0100

    Task

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* 
+* _strncat - Entry point
+* @dest: p
+* @src: p
+* @n: int
 * Return: Always 0 (Success)
 */
 
-int main(void)
+char *_strncat(char *dest, char *src, int n)
 {
-	return (0);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
