e2784d2e274bb61acba301be0e555b40f6cabcea
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-04 11:07:49 +0100

    Task final

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
-* _strchr - Entry point
-* @s: ptr
-* @c: char
-* Return: Always 0 (Success)
-*/
-
+ * _strchr - Entry point
+ * @s: ptr
+ * @c: char
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
+	while (*s && *s != c)
+		s++;
 
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-
-	return (s);
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
