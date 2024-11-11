545fe8926f21a2b5927910d431376d14a41bb14a
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-11 10:34:17 +0100

    Task 0x04

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,12 +1,24 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * free_grid - Entry point
+ * @grid: ptr to ptr of int
+ * @height: int
+ * Return: Always 0 (Success)
+ */
+void free_grid(int **grid, int height)
 {
-	return (0);
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] != NULL)
+			free(grid[i]);
+	}
+
+	free(grid);
 }
