913cbe46c96128ba8bc8983b95b34f7cf9909e0f
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-11 10:23:52 +0100

    Task 0x01

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,12 +1,31 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * _strdup - Entry point
+ * @str: char ptr
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
 {
-	return (0);
+	char *cpy;
+	int len;
+
+	register int index;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len])
+		len++;
+
+	cpy = malloc(len + 1);
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; index <= len; index++)
+		cpy[index] = str[index];
+
+	return (cpy);
 }
