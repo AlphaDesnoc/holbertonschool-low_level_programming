f215920222c4a85f60b95aa065068229fa196a66
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-11 10:51:15 +0100

    Task 0x02

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * _calloc - Entry point
+ * @nmemb: int
+ * @size: int
+ * Return: Always 0 (Success)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (0);
+	unsigned char *block;
+	unsigned int total_size;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	if (total_size / nmemb != size)
+		return (NULL);
+
+	block = malloc(total_size);
+	if (block == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_size; i++)
+		block[i] = 0;
+
+	return ((void *)block);
 }
