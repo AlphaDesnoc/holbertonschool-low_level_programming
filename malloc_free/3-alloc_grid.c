19b3c44a056d626dfeb8d82421e80f7c49888e59
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-11 10:30:53 +0100

    Task 0x03

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,12 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * alloc_grid - Entry point
+ * @width: int
+ * @height: int
+ * Return: Always 0 (Success)
+ */
+int **alloc_grid(int width, int height)
 {
-	return (0);
+	int **array;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+
+	return (array);
 }
