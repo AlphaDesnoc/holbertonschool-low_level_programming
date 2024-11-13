caf9d59316f1c8c478da162e908aae25484140b8
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-13 14:55:12 +0100

    Task 0x02

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,5 +8,14 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %d\nAge: %d\nOwner: %d", d.name, d.age, d.owner);
+	if (!d)
+		return;
+	if (!d->name)
+		d->name = "(nil)";
+	if (!d->owner)
+		d->owner = "(nil)";
+
+	printf("Name: %s", d->name);
+	printf("Age: %f", d->age);
+	printf("Owner: %s", d->owner);
 }
