08c2b8dc4880f91f735dc50f3460da96f375195a
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-18 13:33:29 +0100

    Task

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,22 +1,21 @@
-#include"function_pointers.h"
-#include <stddef.h>
+#include "function_pointers.h"
 
 /**
- * array_iterator - exec a function.MLP
- * @array: pointer to array.
- * @size: size of array.
- * @action: pointer to a function taking an int pointer as parameter.
+ * array_iterator - Executes a function on each element of an array.
+ * @array: The array to iterate over.
+ * @size: The size of the array.
+ * @action: A pointer to the function to be executed on each element.
+ *
+ * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+    if (array == NULL || action == NULL)
+        return;
 
-	if (array != NULL && action != NULL && size > 0)
-	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
-	}
+    for (size_t i = 0; i < size; i++)
+    {
+        action(array[i]);
+    }
 }
+
