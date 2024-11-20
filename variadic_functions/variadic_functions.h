1ad353af1fb89ff25c6c0e3dbb1abaec5b3cf818
root <root@DESKTOP-VS17LS8.localdomain>
2024-11-20 14:12:47 +0100

    Task .

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,14 +1,20 @@
 #ifndef FUNC
 #define FUNC
 
-#include <stdarg.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct formats
+{
+        char *type;
+        void (*function)(va_list);
+} checker;
 
 #endif
